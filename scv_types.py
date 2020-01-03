267047cad9e106fc624e19e597b97f7e01830258
jayanam <jayanam.games@gmail.com>
2020-01-03 14:12:21 +0100

    0.9.1.1 : Mouse wheel friendly names

--- scv_types.py
+++ scv_types.py
@@ -8,6 +8,7 @@ class SCV_Key_Input:
 
     def __init__(self):
         self._friendly_names = { 'LEFTMOUSE': 'Left', 'RIGHTMOUSE': 'Right', 'MIDDLEMOUSE': 'Middle',
+                                 'WHEELUPMOUSE': "Mouse wheel up", "WHEELDOWNMOUSE": "Mouse wheel down",
                                  'ESC': 'Escape', 'RET': 'Enter', 'ONE' : '1', 'TWO': '2', 'THREE' : '3', 'FOUR': '4',
                                  'FIVE': '5', 'SIX':'6', 'SEVEN':'7', 'EIGHT' : '8', 'NINE': '9', 'ZERO' : '0',
                                  'COMMA' : 'Comma', 'PERIOD' : 'Period'}
